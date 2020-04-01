87e7687ab90d06848e2ab85a34201ddda3044aef
Andrea <a.g.baldan@gmail.com>
2020-04-01 16:28:57 +0200

    Add basic echo server

---
+++ src/tts_server.c
@@ -0,0 +1,97 @@
+/* BSD 2-Clause License
+ *
+ * Copyright (c) 2020, Andrea Giacomo Baldan
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ev_tcp.h"
+#include "tts_server.h"
+
+#define BACKLOG 128
+
+static void on_close(ev_tcp_handle *client, int err) {
+    (void) client;
+    if (err == EV_TCP_SUCCESS)
+        printf("Connection closed\n");
+    else
+        printf("Connection closed: %s\n", ev_tcp_err(err));
+    free(client);
+}
+
+static void on_write(ev_tcp_handle *client) {
+    (void) client;
+    printf("Written response\n");
+}
+
+static void on_data(ev_tcp_handle *client) {
+    printf("Received %li bytes\n", client->buffer.size);
+    if (strncmp(client->buffer.buf, "quit", 4) == 0)
+        (void) ev_tcp_enqueue_close(client);
+    else
+        (void) ev_tcp_enqueue_write(client);
+}
+
+static void on_connection(ev_tcp_handle *server) {
+    int err = 0;
+    ev_tcp_handle *client = malloc(sizeof(*client));
+    if ((err = ev_tcp_server_accept(server, client, on_data, on_write)) < 0) {
+        if (err < 0) {
+            if (err == -1)
+                fprintf(stderr, "Error occured: %s\n", strerror(errno));
+            else
+                fprintf(stderr, "Error occured:%s\n", ev_tcp_err(err));
+        }
+        free(client);
+    } else {
+        ev_tcp_handle_set_on_close(client, on_close);
+    }
+}
+
+int tts_start_server(const char *host, int port) {
+    ev_context *ctx = ev_get_ev_context();
+    ev_tcp_server server;
+    ev_tcp_server_init(&server, ctx, BACKLOG);
+    int err = ev_tcp_server_listen(&server, host, port, on_connection);
+    if (err < 0) {
+        if (err == -1)
+            fprintf(stderr, "Error occured: %s\n", strerror(errno));
+        else
+            fprintf(stderr, "Error occured: %s\n", ev_tcp_err(err));
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Listening on %s:%i\n", host, port);
+
+    // Blocking call
+    ev_tcp_server_run(&server);
+
+    // This could be registered to a SIGINT|SIGTERM signal notification
+    // to stop the server with Ctrl+C
+    ev_tcp_server_stop(&server);
+
+    return 0;
+}
